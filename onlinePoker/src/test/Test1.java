86c940043d417d74b15748fb4749ad6d514f43b5
12249940 <12249940@WH268PC9.wayneschools.net>
2024-01-19 10:23:02 -0500

    init

---
+++ onlinePoker/src/test/Test1.java
@@ -0,0 +1,21 @@
+package test;
+
+import processing.core.PApplet;
+
+public class Test1 extends PApplet {
+
+	@Override
+	public void settings() {
+		
+	}
+
+	@Override
+	public void setup() {
+		
+	}
+
+	@Override
+	public void draw() {
+	  
+	}
+}
